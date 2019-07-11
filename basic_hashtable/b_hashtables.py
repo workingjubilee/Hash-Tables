4d21e2bfe89d580a5762fa3f1fd3d5e08e47c06d
workingjubilee <workingjubilee@gmail.com>
2019-07-11 04:21:34 -0700

    Adds basic hashtable

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,15 +15,23 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.storage = capacity * [None]
+        self.size = capacity
+
+
 
 
 # '''
 # Fill this in.
 # Research and implement the djb2 hash function
 # '''
-def hash(string, max):
-    pass
+def hash(string, table_max=1):
+    djb2 = 5381
+    for i in string:
+        i = ord(i)
+        djb2 = djb2 * (2 ** 5) + djb2 + i
+    key = djb2 % table_max
+    return key
 
 
 # '''
@@ -32,7 +40,13 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    pair = Pair(key, value)
+    hashkey = hash(key, hash_table.size)
+    if hash_table.storage[hashkey] is not None:
+        print("Warning, data loss!")
+
+    hash_table.storage[hashkey] = pair
+    return
 
 
 # '''
@@ -41,7 +55,15 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    hashkey = hash(key, hash_table.size)
+
+    if hash_table.storage[hashkey] == None:
+        print("Futile action!")
+
+    hash_table.storage[hashkey] = None
+    return
+
+
 
 
 # '''
@@ -50,11 +72,20 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    hashkey = hash(key, hash_table.size)
+
+    loc = hash_table.storage[hashkey]
+    if loc is not None:
+        return loc.value
+    else:
+        return None
+
 
 
 def Testing():
     ht = BasicHashTable(16)
+
+    hash("line!")
 
     hash_table_insert(ht, "line", "Here today...\n")
 
@@ -65,5 +96,5 @@ def Testing():
     else:
         print("ERROR:  STILL HERE")
 
-
 Testing()
+    
